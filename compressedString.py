12bb8ff1cfb92ea01319c0ac914d9b736a99d957
Ken Huang <54195949+ZixuanHuang1995@users.noreply.github.com>
2025-04-13 07:40:14 +0000

    Question1

---
+++ compressedString.py
@@ -0,0 +1,24 @@
+def compressedString(message):
+    if not message:
+        return ""
+    
+    compressed = []
+    count = 1
+
+    for i in range(1, len(message)):
+        if message[i] == message[i - 1]:
+            count += 1
+        else:
+            compressed.append(message[i - 1])
+            if count > 1:
+                compressed.append(str(count))
+            count = 1
+
+    # 處理最後一個字元或字元串
+    compressed.append(message[-1])
+    if count > 1:
+        compressed.append(str(count))
+        
+    return ''.join(compressed)
+
+print(compressedString("abaasass"))  # Output: "aba2sas2"
